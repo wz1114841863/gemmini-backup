e92e4956d47e02f5977a593ad4739392036fed69
wz1114841863 <18801383735@163.com>
2025-09-22 16:55:54 +0800

    add anno of Util, LocalAddr, ZeroWriter

--- src/main/scala/gemmini/Util.scala
+++ src/main/scala/gemmini/Util.scala
@@ -3,155 +3,183 @@ package gemmini
 import chisel3._
 import chisel3.util._
 
+// 各种工具函数
 object Util {
-  def wrappingAdd(u: UInt, n: UInt, max_plus_one: Int): UInt = {
-    val max = max_plus_one - 1
-    if (max == 0) {
-      0.U
-    } else {
-      assert(n <= max.U, "cannot wrapAdd when n is larger than max")
-      Mux(u >= max.U - n + 1.U && n =/= 0.U, n - (max.U - u) - 1.U, u + n)
-    }
-  }
-
-  def wrappingAdd(u: UInt, n: UInt, max_plus_one: UInt, en: Bool = true.B): UInt = {
-    val max = max_plus_one - 1.U
-    assert(n <= max || max === 0.U, "cannot wrapAdd when n is larger than max, unless max is 0")
-
-    /*
-    Mux(!en, u,
-      Mux (max === 0.U, 0.U,
-        Mux(u >= max - n + 1.U && n =/= 0.U, n - (max - u) - 1.U, u + n)))
-    */
-
-    MuxCase(u + n, Seq(
-      (!en) -> u,
-      (max === 0.U) -> 0.U,
-      (u >= max - n + 1.U && n =/= 0.U) -> (n - (max - u) - 1.U)
-    ))
-  }
-
-  def satAdd(u: UInt, v: UInt, max: UInt): UInt = {
-    Mux(u +& v > max, max, u + v)
-  }
-
-  def floorAdd(u: UInt, n: UInt, max_plus_one: UInt, en: Bool = true.B): UInt = {
-    val max = max_plus_one - 1.U
-
-    MuxCase(u + n, Seq(
-      (!en) -> u,
-      ((u +& n) > max) -> 0.U
-    ))
-  }
-
-  def sFloorAdd(s: SInt, n: UInt, max_plus_one: SInt, min: SInt, en: Bool = true.B): SInt = {
-    val max = max_plus_one - 1.S
-
-    MuxCase(s + n.zext, Seq(
-      (!en) -> s,
-      ((s +& n.zext) > max) -> min
-    ))
-  }
-
-  def wrappingSub(u: UInt, n: UInt, max_plus_one: Int): UInt = {
-    val max = max_plus_one - 1
-    assert(n <= max.U, "cannot wrapSub when n is larger than max")
-    Mux(u < n, max.U - (n-u) + 1.U, u - n)
-  }
-
-  def ceilingDivide(numer: Int, denom: Int): Int = {
-    if (numer % denom == 0) { numer / denom }
-    else { numer / denom + 1}
-  }
-
-  def closestLowerPowerOf2(u: UInt): UInt = {
-    // TODO figure out a more efficient way of doing this. Is this many muxes really necessary?
-    val exp = u.asBools.zipWithIndex.map { case (b, i) =>
-        Mux(b, i.U, 0.U)
-    }.reduce((acc, u) => Mux(acc > u, acc, u))
-
-    (1.U << exp).asUInt
-  }
-
-  def closestAlignedLowerPowerOf2(u: UInt, addr: UInt, stride: UInt, rowBytes: Int): UInt = {
-    val lgRowBytes = log2Ceil(rowBytes)
-
-    // TODO figure out a more efficient way of doing this. Is this many muxes really necessary?
-    val exp = u.asBools.zipWithIndex.map { case (b, i) =>
-      Mux(b && addr(i + lgRowBytes - 1, 0) === 0.U && stride(i + lgRowBytes - 1, 0) === 0.U, i.U, 0.U)
-    }.reduce((acc, u) => Mux(acc > u, acc, u))
-
-    (1.U << exp).asUInt
-  }
-
-  // This function will return "next" with a 0-cycle delay when the "enable" signal is high. It's like a queue with
-  // the "pipe" and "flow" parameters set to "true"
-  def RegEnableThru[T <: Data](next: T, enable: Bool): T = {
-    val buf = RegEnable(next, enable)
-    Mux(enable, next, buf)
-  }
-
-  def RegEnableThru[T <: Data](next: T, init: T, enable: Bool): T = {
-    val buf = RegEnable(next, init, enable)
-    Mux(enable, next, buf)
-  }
-
-  def maxOf(u1: UInt, u2: UInt): UInt = {
-    Mux(u1 > u2, u1, u2)
-  }
-
-  def maxOf[T <: Data](x: T, y: T)(implicit ev: Arithmetic[T]): T = {
-    import ev._
-    Mux(x > y, x, y)
-  }
-
-  def minOf(u1: UInt, u2: UInt): UInt = {
-    Mux(u1 < u2, u1, u2)
-  }
-
-  def accumulateTree[T <: Data](xs: Seq[T])(implicit ev: Arithmetic[T]): T = {
-    import ev._
-
-    assert(xs.nonEmpty, "can't accumulate 0 elements")
-
-    if (xs.length == 1) {
-      xs.head
-    } else {
-      val upperRowLen = 1 << log2Ceil(xs.length)
-      val upperRow = xs.padTo(upperRowLen, xs.head.zero)
-      val pairs = upperRow.grouped(2)
-      val lowerRow = pairs.map { case Seq(a, b) => a + b }
-      accumulateTree(lowerRow.toSeq)
-    }
-  }
-
-  // An undirectioned Valid bundle
-  class UDValid[T <: Data](t: T) extends Bundle {
-    val valid = Bool()
-    val bits = t.cloneType
-
-    def push(b: T): Unit = {
-      valid := true.B
-      bits := b
+    // 回绕加法
+    def wrappingAdd(u: UInt, n: UInt, max_plus_one: Int): UInt = {
+        val max = max_plus_one - 1
+        if (max == 0) {
+            0.U
+        } else {
+            assert(n <= max.U, "cannot wrapAdd when n is larger than max")
+            Mux(u >= max.U - n + 1.U && n =/= 0.U, n - (max.U - u) - 1.U, u + n)
+        }
     }
 
-    def pop(dummy: Int = 0): T = {
-      valid := false.B
-      bits
+    // 回绕加法,带使能信号
+    def wrappingAdd(u: UInt, n: UInt, max_plus_one: UInt, en: Bool = true.B): UInt = {
+        val max = max_plus_one - 1.U
+        assert(n <= max || max === 0.U, "cannot wrapAdd when n is larger than max, unless max is 0")
+
+        /*
+        Mux(!en, u,
+            Mux (max === 0.U, 0.U,
+            Mux(u >= max - n + 1.U && n =/= 0.U, n - (max - u) - 1.U, u + n)))
+         */
+
+        // MuxCase(default, Array(c1 -> a, c2 -> b, ...))
+        MuxCase(
+            u + n,
+            Seq(
+                (!en) -> u,
+                (max === 0.U) -> 0.U,
+                (u >= max - n + 1.U && n =/= 0.U) -> (n - (max - u) - 1.U)
+            )
+        )
     }
 
-  }
+    // 饱和加法, 防止溢出, +&(加宽加法, 防止溢出截断)
+    def satAdd(u: UInt, v: UInt, max: UInt): UInt = {
+        Mux(u +& v > max, max, u + v)
+    }
 
-  object UDValid {
-    def apply[T <: Data](t: T): UDValid[T] = new UDValid(t)
-  }
+    // 回零加法, 加法后如果超过max, 则回零
+    def floorAdd(u: UInt, n: UInt, max_plus_one: UInt, en: Bool = true.B): UInt = {
+        val max = max_plus_one - 1.U
 
-  // creates a Reg and the next-state Wire, and returns both
-  def regwire(bits: Int) = {
-    val wire = Wire(UInt(bits.W))
-    val reg = RegNext(wire)
-    wire := reg // default wire to read from reg
-    (reg, wire)
-  }
+        MuxCase(
+            u + n,
+            Seq(
+                (!en) -> u,
+                ((u +& n) > max) -> 0.U
+            )
+        )
+    }
 
+    // 回零加法, 带符号数
+    def sFloorAdd(s: SInt, n: UInt, max_plus_one: SInt, min: SInt, en: Bool = true.B): SInt = {
+        val max = max_plus_one - 1.S
+
+        MuxCase(
+            s + n.zext,
+            Seq(
+                (!en) -> s,
+                ((s +& n.zext) > max) -> min
+            )
+        )
+    }
+
+    // 回绕减法
+    def wrappingSub(u: UInt, n: UInt, max_plus_one: Int): UInt = {
+        val max = max_plus_one - 1
+        assert(n <= max.U, "cannot wrapSub when n is larger than max")
+        Mux(u < n, max.U - (n - u) + 1.U, u - n)
+    }
+
+    // 向上取整除法, ceilingDivide(10, 3) = 4
+    def ceilingDivide(numer: Int, denom: Int): Int = {
+        if (numer % denom == 0) { numer / denom }
+        else { numer / denom + 1 }
+    }
+
+    // 找到小于等于 u 的最大 2 的幂
+    def closestLowerPowerOf2(u: UInt): UInt = {
+        // TODO figure out a more efficient way of doing this. Is this many muxes really necessary?
+        // 找到最高有效位索引
+        val exp = u.asBools.zipWithIndex
+            .map { case (b, i) =>
+                Mux(b, i.U, 0.U)
+            }
+            .reduce((acc, u) => Mux(acc > u, acc, u))
+
+        (1.U << exp).asUInt
+    }
+
+    // 在满足地址对齐和 stride 对齐的前提下,找到最大 2 的幂
+    def closestAlignedLowerPowerOf2(u: UInt, addr: UInt, stride: UInt, rowBytes: Int): UInt = {
+        // 计算对齐位宽, rowBytes是2的幂, 所以低lgRowBytes位为0即表示"对齐"
+        val lgRowBytes = log2Ceil(rowBytes)
+
+        // TODO figure out a more efficient way of doing this. Is this many muxes really necessary?
+        val exp = u.asBools.zipWithIndex
+            .map { case (b, i) =>
+                Mux(b && addr(i + lgRowBytes - 1, 0) === 0.U && stride(i + lgRowBytes - 1, 0) === 0.U, i.U, 0.U)
+            }
+            .reduce((acc, u) => Mux(acc > u, acc, u))
+
+        (1.U << exp).asUInt
+    }
+
+    // This function will return "next" with a 0-cycle delay when the "enable" signal is high. It's like a queue with
+    // the "pipe" and "flow" parameters set to "true"
+    def RegEnableThru[T <: Data](next: T, enable: Bool): T = {
+        val buf = RegEnable(next, enable)
+        Mux(enable, next, buf)
+    }
+
+    def RegEnableThru[T <: Data](next: T, init: T, enable: Bool): T = {
+        val buf = RegEnable(next, init, enable)
+        Mux(enable, next, buf)
+    }
+
+    def maxOf(u1: UInt, u2: UInt): UInt = {
+        Mux(u1 > u2, u1, u2)
+    }
+
+    // 泛型, 支持任何实现了Arithmetic类型类的类型
+    def maxOf[T <: Data](x: T, y: T)(implicit ev: Arithmetic[T]): T = {
+        import ev._
+        Mux(x > y, x, y)
+    }
+
+    def minOf(u1: UInt, u2: UInt): UInt = {
+        Mux(u1 < u2, u1, u2)
+    }
+
+    // 将一组数用二叉树方式累加,减少加法器延迟,
+    // 调用 accumulateTree 的地方会生成一个树形加法器
+    def accumulateTree[T <: Data](xs: Seq[T])(implicit ev: Arithmetic[T]): T = {
+        import ev._
+
+        assert(xs.nonEmpty, "can't accumulate 0 elements")
+
+        if (xs.length == 1) {
+            xs.head
+        } else {
+            val upperRowLen = 1 << log2Ceil(xs.length)
+            val upperRow = xs.padTo(upperRowLen, xs.head.zero)
+            val pairs = upperRow.grouped(2)
+            val lowerRow = pairs.map { case Seq(a, b) => a + b }
+            accumulateTree(lowerRow.toSeq)
+        }
+    }
+
+    // An undirectioned Valid bundle
+    class UDValid[T <: Data](t: T) extends Bundle {
+        val valid = Bool()
+        val bits = t.cloneType
+
+        def push(b: T): Unit = {
+            valid := true.B
+            bits := b
+        }
+
+        def pop(dummy: Int = 0): T = {
+            valid := false.B
+            bits
+        }
+
+    }
+
+    object UDValid {
+        def apply[T <: Data](t: T): UDValid[T] = new UDValid(t)
+    }
+
+    // creates a Reg and the next-state Wire, and returns both
+    def regwire(bits: Int) = {
+        val wire = Wire(UInt(bits.W))
+        val reg = RegNext(wire)
+        wire := reg // default wire to read from reg
+        (reg, wire)
+    }
 }
--- src/main/scala/gemmini/ZeroWriter.scala
+++ src/main/scala/gemmini/ZeroWriter.scala
@@ -1,4 +1,3 @@
-
 package gemmini
 
 import chisel3._
@@ -7,70 +6,78 @@ import chisel3.util._
 import Util._
 
 class ZeroWriterReq[Tag <: Data](laddr_t: LocalAddr, max_cols: Int, tag_t: Tag) extends Bundle {
-  val laddr = laddr_t
-  val cols = UInt(log2Up(max_cols+1).W)
-  val block_stride = UInt(16.W) // TODO magic number
-  val tag = tag_t
-
+    val laddr = laddr_t // 起始地址
+    val cols = UInt(log2Up(max_cols + 1).W) // 要清零的列数
+    val block_stride = UInt(16.W) // 块之间的步长, TODO magic number
+    val tag = tag_t // 请求的标签, 用于区分不同的请求
 }
 
 class ZeroWriterResp[Tag <: Data](laddr_t: LocalAddr, block_cols: Int, tag_t: Tag) extends Bundle {
-  val laddr = laddr_t.cloneType
-  val mask = Vec(block_cols, Bool())
-  val last = Bool()
-  val tag = tag_t
-
+    val laddr = laddr_t.cloneType // 当前块的起始地址
+    val mask = Vec(block_cols, Bool()) // 哪些列需要被清零
+    val last = Bool() // 是否是最后一个块
+    val tag = tag_t // 请求的标签,回传
 }
 
+/*
+    ZeroWriter的作用是 将一块内存区域清零, 通常用于初始化输出缓冲区或中间结果缓冲区.
+        接收一个请求清零(一片连续地址)的信号,这个模块会计算并并转发(但是不实际进行操作)对应bank的清零指令
+ */
 class ZeroWriter[T <: Data, U <: Data, V <: Data, Tag <: Data](config: GemminiArrayConfig[T, U, V], tag_t: Tag)
-  extends Module {
-  import config._
+    extends Module {
+    import config._
 
-  val block_cols = meshColumns * tileColumns
-  val max_cols = (dma_maxbytes / (inputType.getWidth / 8)) max block_cols
+    val block_cols = meshColumns * tileColumns
+    val max_cols = (dma_maxbytes / (inputType.getWidth / 8)) max block_cols
 
-  val io = IO(new Bundle {
-    val req = Flipped(Decoupled(new ZeroWriterReq(local_addr_t, max_cols, tag_t)))
-    val resp = Decoupled(new ZeroWriterResp(local_addr_t, block_cols, tag_t))
-  })
+    val io = IO(new Bundle {
+        val req = Flipped(Decoupled(new ZeroWriterReq(local_addr_t, max_cols, tag_t)))
+        val resp = Decoupled(new ZeroWriterResp(local_addr_t, block_cols, tag_t))
+    })
 
-  val req = Reg(UDValid(new ZeroWriterReq(local_addr_t, max_cols, tag_t)))
+    // 当前正在处理的请求
+    val req = Reg(UDValid(new ZeroWriterReq(local_addr_t, max_cols, tag_t)))
+    // 当前处理到的列索引, 用于跟踪进度
+    val col_counter = Reg(UInt(log2Up(max_cols).W))
 
-  val col_counter = Reg(UInt(log2Up(max_cols).W))
+    io.req.ready := !req.valid
 
-  io.req.ready := !req.valid
-
-  io.resp.valid := req.valid
-  io.resp.bits.laddr := req.bits.laddr + req.bits.block_stride * {
-    // This code block was originally just "col_counter / block_cols.U". We
-    // changed it to satisfy Verilator's linter
-    if (col_counter.getWidth >= log2Ceil(block_cols+1))
-      (col_counter / block_cols.U(col_counter.getWidth.W))
-    else
-      0.U
-  }
-  io.resp.bits.mask.zipWithIndex.foreach { case (m, i) => m := col_counter + i.U < req.bits.cols }
-  io.resp.bits.last := col_counter +& block_cols.U >= req.bits.cols
-  io.resp.bits.tag := req.bits.tag
-
-  when (io.resp.fire) {
-    val next_col_counter = floorAdd(col_counter, block_cols.U, req.bits.cols)
-
-    col_counter := next_col_counter
-
-    when (next_col_counter === 0.U) {
-      req.pop()
-      io.req.ready := true.B
+    io.resp.valid := req.valid
+    // 计算当前块的地址偏移量, 使用col_counter / block_cols来确定当前是第几个块
+    io.resp.bits.laddr := req.bits.laddr + req.bits.block_stride * {
+        // This code block was originally just "col_counter / block_cols.U". We
+        // changed it to satisfy Verilator's linter
+        if (col_counter.getWidth >= log2Ceil(block_cols + 1))
+            (col_counter / block_cols.U(col_counter.getWidth.W))
+        else
+            0.U
     }
-  }
+    // 对当前块的每一列, 判断是否需要清零(mask为true表示需要清零)
+    io.resp.bits.mask.zipWithIndex.foreach { case (m, i) => m := col_counter + i.U < req.bits.cols }
+    // 判断是否是最后一个块
+    io.resp.bits.last := col_counter +& block_cols.U >= req.bits.cols
+    io.resp.bits.tag := req.bits.tag
 
-  when (io.req.fire) {
-    req.push(io.req.bits)
+    when(io.resp.fire) {
+        // floorAdd是一个自定义函数,实现"循环加法",即不超过 cols
+        val next_col_counter = floorAdd(col_counter, block_cols.U, req.bits.cols)
 
-    col_counter := 0.U
-  }
+        col_counter := next_col_counter
 
-  when (reset.asBool) {
-    req.pop()
-  }
+        when(next_col_counter === 0.U) {
+            req.pop()
+            io.req.ready := true.B
+        }
+    }
+
+    // 当有新请求到来时,保存请求并重置列计数器
+    when(io.req.fire) {
+        req.push(io.req.bits)
+
+        col_counter := 0.U
+    }
+
+    when(reset.asBool) {
+        req.pop()
+    }
 }
